13a35dfd56bca8801438fe5b0fd53f3a41126248
Hanna Negash <hnegash.n@gmail.com>
2024-09-01 18:46:21 -0700

    Add temporarily code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("...")
+      .then(res => res.json())
+      .then(setData());
+  }, []);
+
   <div>
     <h1>Colerful Connections in Progress</h1>
+
+    {data.map(item => (
+      <div key={item.id}>{item.colorName}</div>
+    ))}
   </div>;
 }
