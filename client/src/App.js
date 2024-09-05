9976f9f1a08c8482b9aca32efe10c1565048b338
Hanna Negash <hnegash.n@gmail.com>
2024-09-04 18:10:47 -0700

    Add incomplete code for POST method

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,35 @@ import { useState, useEffect } from "react";
 
 export default function App() {
   const URL = "http://localhost:3001/colorOptions";
-  const [color, setColor] = useState([]);
 
+  const [color, setColor] = useState([]);
+  const [name, setName] = useState("");
+  const [colorCode, setcolorCode] = useState("");
+  const [something, setSomething] = useState("");
+
+  //A GET request to get all data
   useEffect(() => {
     fetch(URL)
       .then(res => res.json())
       .then(color => setColor(color));
   });
+
+  const handleSubmit = e => {
+    e.preventDefault(); // prevent browser from reloading and get the data on time
+
+    const data = { name, colorCode, something };
+
+    // A POST to add data
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" }, // we want to get the response data in that format
+      body: JSON.stringify(data), // defines the body data that we want to send to the server -- convert to JSON
+    };
+
+    fetch(URL, requestOptions)
+      .then(response => response.json())
+      .then(res => console.log(res)); // response is shown on the console window
+  };
 
   return (
     <div className="App">
